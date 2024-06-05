6157603898c2ebd0cf9bbe9633d3a86f775d9194
Brody Blair <brocbla@umich.edu>
2024-06-05 12:32:08 -0500

    refactor tailwind/remove unused directory

--- src/lib/base-class/index.ts
+++
@@ -1,5 +0,0 @@
-import { BaseGame } from './baseGame';
-import { BaseRack } from './baseRack';
-import { BasePlayer } from './basePlayer';
-
-export { BaseGame, BaseRack, BasePlayer };
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,8 +5,8 @@
 
 <div class="grid grid-cols-4 place-items-center max-w-[600px] m-auto relative">
 	<div class="col-span-1"></div>
-	<img src={logo} alt="rack em up" class="my-2 col-start-2 col-end-4" />
-	<div class="col-start-4">
+	<img src={logo} alt="rack em up" class="my-2 col-span-2" />
+	<div>
 		<Hamburger />
 	</div>
 </div>
