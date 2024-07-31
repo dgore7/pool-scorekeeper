777d61d5c615b82bd0666dca7604a7a6130756be
dgore7 <dgore7@gmail.com>
2024-07-30 21:14:22 -0500

    update bg image

--- src/lib/components/RuleForm.svelte
+++ src/lib/components/RuleForm.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { GameType, RuleType } from '$lib/types';
-	import MainBackgroundIcon from './icons/MainBackgroundIcon.svelte';
+	import splash from '../assets/rules-panel.svg';
 	import Select from './Select.svelte';
 
 	const poolGames = [
@@ -22,13 +22,13 @@
 
 <div
 	class="relative flex flex-col flex-grow gap-6 p-4 rounded-2xl justify-end overflow-hidden"
-	style="background:linear-gradient(40deg in oklab, black, #002B48 40%, #4FBEFD);"
+	style:background-image="linear-gradient(to bottom, #28282c, #16161a)"
 >
-	<div class="absolute pointer-events-none scale-125 top-2 left-5">
-		<MainBackgroundIcon />
-	</div>
-
-	<div>
+	<div
+		id="rules-form-splash"
+		style:--splash="url({splash})"
+		class="flex-1 -m-4 mb-0 p-4 content-end relative"
+	>
 		<h4 class="text-xl color-[#D5D5D5]">POOL SCOREKEEPER</h4>
 		<h1 class="text-5xl">Rack'em Up!</h1>
 	</div>
@@ -43,3 +43,27 @@
 		<Select bind:selectedItem={selectedRules} options={rules} label={'Choose Rules'} name="rules" />
 	</fieldset>
 </div>
+
+<style>
+	#rules-form-splash::before {
+		content: '';
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		inset: 0;
+		background-image: var(--splash);
+		background-size: 100% 110%;
+		background-repeat: no-repeat;
+		background-position: center;
+	}
+
+	#rules-form-splash {
+		background-image: linear-gradient(to bottom, #1c1c2000, #1c1c20ff),
+			linear-gradient(220deg in oklab, #4fbefd88 0%, #0096ed88 19%, #002b4888 72%, #001a2988 92%);
+		background-position: bottom, top;
+		background-size:
+			100% 45%,
+			160% 100%;
+		background-repeat: no-repeat, no-repeat;
+	}
+</style>
