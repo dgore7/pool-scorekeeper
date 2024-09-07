ef42d4b50131af22367d2afad01b94cffc4b86f3
David Gorelik <dgore7@gmail.com>
2024-09-07 13:55:50 -0500

    format

--- src/lib/components/icons/CloseIcon.svelte
+++ src/lib/components/icons/CloseIcon.svelte
@@ -5,7 +5,19 @@
 	export { className as class };
 </script>
 
-<svg {width} class={className} height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<path d="M5 15L14 5M5 5L15 15" stroke="#AFAFAA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-	</svg>
-	
+<svg
+	{width}
+	class={className}
+	height="20"
+	viewBox="0 0 20 20"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M5 15L14 5M5 5L15 15"
+		stroke="#AFAFAA"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
